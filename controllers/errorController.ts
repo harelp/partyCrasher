d2189785afda122b9d403d1c49414e4f1f9748e3
“Harel <harelperi2@gmail.com>
2022-08-23 23:21:39 -0400

    added unhandler funcs

--- controllers/errorController.ts
+++ controllers/errorController.ts
@@ -2,8 +2,22 @@ export = (err: any, req: any, res: any, next: any) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (err.name === 'CastError') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: 'Invalid Id',
+    });
+  } else if (err.code === 11000) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: 'Already In Use, Please select a different value',
+    });
+  } else {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: err,
+      stack: err.stack,
+    });
+  }
 };
--- server.ts
+++ server.ts
@@ -2,6 +2,11 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import app from './app';
 
+process.on('uncaughtException', (err: any) => {
+  console.log('UNHANDLED EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 // config file and database
 dotenv.config({ path: './config.env' });
 const DB: any = process.env.DATABASE_URL;
@@ -13,6 +18,14 @@ mongoose.connect(DB).then(() => {
 
 // open server
 const server = app.listen(process.env.PORT, () => {
-  const date = new Date().toISOString();
+  const date: string = new Date().toISOString();
   console.log(`Started server at ${date} on port ${process.env.PORT}`);
+});
+
+process.on('unhandledRejection', (err: any) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
